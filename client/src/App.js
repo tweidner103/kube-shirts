cfb7f8c866c8d57990e355d13f79fb5e4079ef55
tweidner103 <tcwcode20@gmail.com>
2022-10-28 14:43:04 -0700

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-            Fib Calculator
+            Fib Calculator vewrsion 2.0
           </a>
           <Link to='/'>Home</Link>
           <Link to='/otherpage'>Other Page</Link>
